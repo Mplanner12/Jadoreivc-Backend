ef2484f5a09a1cdfb028f570baeced90d56a132d
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-09-18 11:35:15 +0100

    Paypal Integration, Checkout page

--- controllers/tourPlanController.js
+++ controllers/tourPlanController.js
@@ -2,36 +2,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// exports.createTourPlan = async (req, res) => {
-//   const {
-//     touristId,
-//     location,
-//     startDate,
-//     endDate,
-//     time,
-//     numberOfPeople,
-//     guidePreference,
-//   } = req.body;
-
-//   try {
-//     const tourPlan = await prisma.tourPlan.create({
-//       data: {
-//         touristId,
-//         location,
-//         startDate,
-//         endDate,
-//         time,
-//         numberOfPeople,
-//         guidePreference,
-//       },
-//     });
-
-//     res.status(201).json({ success: true, tourPlan });
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error", error: error.message });
-//   }
-// };
-
 exports.createTourPlan = async (req, res) => {
   const {
     touristId,
