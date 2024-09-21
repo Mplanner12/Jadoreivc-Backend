ef3e56e7587ae195dff13dc889c6058fbb962be6
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-09-21 09:05:36 +0100

    change

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,39 +3,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// const protect = async (req, res, next) => {
-//   let token;
-
-//   if (req.cookies.token) {
-//     try {
-//       token = req.cookies.token;
-
-//       // Verify token
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//       // Get user from the token
-//       req.user = await prisma.user.findUnique({
-//         where: { id: decoded.id },
-//       });
-
-//       if (!req.user) {
-//         return res
-//           .status(401)
-//           .json({ message: "Not authorized, user not found" });
-//       }
-
-//       next();
-//     } catch (error) {
-//       console.error(error);
-//       res.status(401).json({ message: "Not authorized, token failed" });
-//     }
-//   }
-
-//   if (!token) {
-//     res.status(401).json({ message: "Not authorized, no token" });
-//   }
-// };
-
 const protect = async (req, res, next) => {
   const token = req.cookies.token;
 
