e2b492b3687a383e295b49964480a3b7143ececc
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-09-28 11:33:41 +0100

    c

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -7,12 +7,12 @@ const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const prisma = new PrismaClient();
 
 const cookieToken = (res, user) => {
-  const token = getJwtToken(user.id); // Generate JWT token
+  const token = getJwtToken(user.id);
 
   const options = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // 3 days
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Send secure cookie in production
+    secure: process.env.NODE_ENV === "production",
     sameSite: "None",
   };
 
